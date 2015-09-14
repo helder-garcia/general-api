762591f0f2241f432346d6127a2c9eaabb13a0ba
Helder Luiz Bezerra de Oliveira Garcia <helder.garcia@serpro.gov.br>
2015-09-14 15:07:26 -0300

    Working on REGEX.

--- api/adapters/lib/sails-adsmAdapter.js
+++ api/adapters/lib/sails-adsmAdapter.js
@@ -88,12 +88,14 @@ module.exports = (function () {
 
     find: function (conn, coll, options, cb) {
       var result =[];
-	  var nodeRegex = new RegExp(/(\w+)[\s|\t]+.*/);
+	  //var nodeRegex = new RegExp(/(\w+)[\s|\t]+.*/);
 	  var myRegex = new RegExp(/(\w+)[\s|\t]+\(\?\)[\s|\t]+(\w+)[\s|\t]+.*/);
-      var cmd = this.defaults.commandPath + this.defaults.commandName + ' -se=' + this.defaults.serverName + ' -id=web_service -password=web_service -dataonly=yes "select NODE_NAME, PLATFORM_NAME, DOMAIN_NAME, ARCHDELETE, BACKDELETE, LOCKED, MAX_MP_ALLOWED from nodes"';
+	  var myRegex = new RegExp(/(\w+)[\s|\t]+(\w+)[\s|\t]+.*/);
+	  //var myRegex = /\w+/g;
+      var cmd = this.defaults.commandPath + this.defaults.commandName + ' -se=' + this.defaults.serverName + ' -id=web_service -password=web_service -dataonly=yes "select NODE_NAME, DOMAIN_NAME, ARCHDELETE, BACKDELETE, LOCKED, MAX_MP_ALLOWED, PLATFORM_NAME from nodes"';
       var child = shell.exec(cmd, {async: true});
       child.stdout.on('data', function(data) {
-    	  arrayOfLines = data.match(/[^\r\n]+/g);  
+    	  arrayOfLines = data.match(/[^\r\n]+/g);
     	  
     	  for (var i = 0, len = arrayOfLines.length; i < len; i++) { 		  
     		  if (arrayOfLines[i].match(/^ANS\d\d\d\d/)) { continue; }
